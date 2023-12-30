24e44d1ad0c0bc826f123997e9bb81be491b134d
Ben Hopkins <ben@hopkins.tech>
2023-12-30 18:48:51 +0000

    Added navigation and some routes

---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,4 @@
+<a href="/">Home</a>
+<a href="/about">About</a>
+<a href="/experience">Experience</a>
+<a href="/contact">Contact</a>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+	import Navigation from '$lib/components/Navigation.svelte';
+</script>
+
+<div>
+	<Navigation />
+
+	<main>
+		<slot />
+	</main>
+</div>
