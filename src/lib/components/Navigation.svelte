24c92e80e634f89764441b6b9ee1d798e41c9271
Ben Hopkins <ben@hopkins.tech>
2024-01-03 19:54:40 +0000

    Basic navigation setup and styling

--- src/lib/components/Navigation.svelte
+++ src/lib/components/Navigation.svelte
@@ -1,8 +1,79 @@
-<header class="absolute flex w-screen justify-end">
-	<nav class="p-4">
-		<a href="/" class="p-4">Home</a>
-		<a href="/about" class="p-4">About</a>
-		<a href="/experience" class="p-4">Experience</a>
-		<a href="/contact" class="p-4">Contact</a>
+<script>
+	const links = [
+		{ text: 'Home', href: '/' },
+		{ text: 'About', href: '/about' },
+		{ text: 'Experience', href: '/experience' },
+		{ text: 'Contact', href: '/contact' }
+	];
+
+	let menuOpen = false;
+
+	function toggleMenu() {
+		menuOpen = !menuOpen;
+	}
+</script>
+
+<header>
+	<nav>
+		<div>BH</div>
+		<div class="nav-links">
+			{#each links as { text, href }}
+				<a {href}>{text}</a>
+			{/each}
+		</div>
+		<button on:click={toggleMenu}> ☰ </button>
 	</nav>
+
+	{#if menuOpen}
+		<div class="drawer">
+			<button on:click={toggleMenu}> X </button>
+			<div class="drawer-links">
+				{#each links as { text, href }}
+					<a {href} on:click={toggleMenu}>{text}</a>
+				{/each}
+			</div>
+		</div>
+	{/if}
 </header>
+
+<style lang="postcss">
+	header {
+		@apply absolute w-screen;
+	}
+
+	nav {
+		@apply bg-red-600 p-4 text-white flex justify-between items-center;
+	}
+
+	nav button {
+		@apply md:hidden cursor-pointer;
+	}
+
+	.nav-links {
+		@apply hidden md:flex;
+	}
+
+	.nav-links a {
+		@apply text-white mr-4 hover:text-gray-300;
+	}
+
+	.nav-links a:last-child {
+		@apply mr-0;
+	}
+
+	.drawer {
+		@apply md:hidden fixed right-0 top-0 h-screen w-screen bg-gray-300 flex justify-center items-center;
+	}
+
+	.drawer button {
+		@apply absolute top-4 right-4 cursor-pointer;
+	}
+
+	.drawer-links {
+		@apply absolute w-3/4;
+	}
+
+	.drawer-links a {
+		@apply block hover:text-red-600 text-left text-4xl mb-8;
+	}
+</style>
