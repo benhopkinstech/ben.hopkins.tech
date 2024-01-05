a4636338c1836d7db22869f5132f401e3d789049
Ben Hopkins <ben@hopkins.tech>
2024-01-05 22:56:16 +0000

    Split custom toggle into its own component

---
+++ src/lib/components/NavigationToggle.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	export let toggleFullNavigation: () => void;
+	export let showFullNavigation: boolean;
+
+	let scrollbarWidth = 0;
+
+	onMount(() => {
+		scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+	});
+</script>
+
+<button
+	on:click={toggleFullNavigation}
+	class:full-nav={showFullNavigation}
+	style="margin-right: {showFullNavigation ? scrollbarWidth : 0}px"
+>
+	<div></div>
+</button>
+
+<style lang="postcss">
+	button {
+		@apply py-[27px] px-4 md:hidden cursor-pointer ml-auto;
+	}
+
+	div {
+		@apply relative w-4 h-0.5 bg-black transition-all ease-in-out duration-500;
+	}
+
+	div::before,
+	div::after {
+		@apply absolute left-0 content-[''] w-full h-0.5 bg-black transition-all ease-in-out duration-500;
+	}
+
+	div::before {
+		@apply transform -translate-y-1.5;
+	}
+
+	div::after {
+		@apply transform translate-y-1.5;
+	}
+
+	.full-nav div {
+		@apply transform -translate-x-10 bg-transparent;
+	}
+
+	.full-nav div::before {
+		@apply transform rotate-45 translate-x-10 translate-y-0;
+	}
+
+	.full-nav div::after {
+		@apply transform -rotate-45 translate-x-10 translate-y-0;
+	}
+</style>
--- src/lib/components/NavigationTop.svelte
+++ src/lib/components/NavigationTop.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import NavigationToggle from '$lib/components/NavigationToggle.svelte';
 
 	export let links: Link[];
 	export let scrollToElement: (selector: string) => void;
@@ -10,12 +10,6 @@
 		text: string;
 		selector: string;
 	};
-
-	let scrollbarWidth = 0;
-
-	onMount(() => {
-		scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
-	});
 </script>
 
 <header>
@@ -25,13 +19,7 @@
 				<a href={'#'} on:click|preventDefault={() => scrollToElement(selector)}>{text}</a>
 			{/each}
 		</div>
-		<button
-			on:click={toggleFullNavigation}
-			class:full-nav={showFullNavigation}
-			style="margin-right: {showFullNavigation ? scrollbarWidth : 0}px"
-		>
-			<div class="nav-toggle"></div>
-		</button>
+		<NavigationToggle {showFullNavigation} {toggleFullNavigation} />
 	</nav>
 </header>
 
@@ -42,39 +30,6 @@
 
 	nav {
 		@apply flex justify-center;
-	}
-
-	nav button {
-		@apply py-[27px] px-4 md:hidden cursor-pointer ml-auto;
-	}
-
-	.nav-toggle {
-		@apply relative w-4 h-0.5 bg-black transition-all ease-in-out duration-500;
-	}
-
-	.nav-toggle::before,
-	.nav-toggle::after {
-		@apply absolute left-0 content-[''] w-full h-0.5 bg-black transition-all ease-in-out duration-500;
-	}
-
-	.nav-toggle::before {
-		@apply transform -translate-y-1.5;
-	}
-
-	.nav-toggle::after {
-		@apply transform translate-y-1.5;
-	}
-
-	.full-nav .nav-toggle {
-		@apply transform -translate-x-10 bg-transparent;
-	}
-
-	.full-nav .nav-toggle::before {
-		@apply transform rotate-45 translate-x-10 translate-y-0;
-	}
-
-	.full-nav .nav-toggle::after {
-		@apply transform -rotate-45 translate-x-10 translate-y-0;
 	}
 
 	.nav-links {
